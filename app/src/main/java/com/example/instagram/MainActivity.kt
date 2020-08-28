11411c014ae3a9da844fccdcaa330403e09f2331
hrohitgupta <59010209+hrohitgupta@users.noreply.github.com>
2020-08-28 15:56:25 +0530

    lest start Instagram clone part 1

--- app/src/main/java/com/example/instagram/MainActivity.kt
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -7,20 +7,47 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                //moveToFragment(HomeFragment())
+                message.text = "Home"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                //moveToFragment(SearchFragment())
+                message.text = "Search"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                message.text = "Add_Post"
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                message.text = "Notifications"
+                //moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                //moveToFragment(ProfileFragment())
+                message.text = "Profile"
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
